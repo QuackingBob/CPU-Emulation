98fcffc0831163d77d3597a402c6c88bc46fa682
QuackingBob <quackingbob101@gmail.com>
2023-12-21 21:51:32 -0800

    update main

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@ int main()
 {
     int clock = 0; 
 
-    for(int i = 0; i < 3000; i++)
+    for(int i = 0; i < 3000; i++) {
+
+        
+
+        if(clock == 0) {
+            clock = 1; 
+        } else {
+            clock = 0;
+        }
+    }
     return 0;
 }
