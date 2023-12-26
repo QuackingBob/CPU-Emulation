4c763e6a156f1296cb5c162f1e036a725b803732
QuackingBob <quackingbob101@gmail.com>
2023-12-26 11:23:11 -0800

    more small changes

--- decoder.cpp
+++ decoder.cpp
@@ -3,24 +3,17 @@
 
 void decode(reg address)
 {
-
-    for(int i = 0; i < MAX_INT; i++) {
-
+    for (int i = 0; i < MAX_INT; i++)
+    {
         reg temp = address;
-
-        for(int j = BUS_SIZE - 1; j >= 0; j--) {
-
+        for (int j = BUS_SIZE - 1; j >= 0; j--)
+        {
             int result = i & 0x00000001;
-
-            if(!result) {
-
+            if (!result)
+            {
                 set_bit(temp, j, not_gate(get_bit(temp, j)));
-
             }
-
-
         }
-
         decoded[i] = and16(temp);
     }
 }
